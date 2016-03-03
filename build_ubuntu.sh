c0b2dfab932f1a51188bee1c71cc9213c9c0dbd3
rolivieri <roliv@us.ibm.com>
2016-03-03 14:28:17 -0600

    IBM-Swift/Kitura#165 New files to support CI builds.

--- build_ubuntu.sh
+++ build_ubuntu.sh
@@ -23,10 +23,39 @@
 # If any commands fail, we want the shell script to exit immediately.
 set -e
 
-# Build swift package
+# Determine platform/OS
+echo "os: $(uname)"
+if [ "$(uname)" == "Darwin" ]; then
+    # Do something under Mac OS X platform
+    echo "os x"
+elif [ "$(expr substr $(uname -s) 1 5)" == "Linux" ]; then
+    # Do something under GNU/Linux platform
+    echo "linux"
+else
+    echo "Unsupported platform!"
+    exit 1
+fi
+
+# Make the working directory the folder from which the script was sourced
 cd "$(dirname "$0")"
+
+# Get project name from project folder
+currentDir=`pwd`
+projectName="$(basename $currentDir)"
+echo "projectName: $projectName"
+
+# Build swift package
 swift build -Xcc -fblocks
-# swift build -Xcc -fblocks -Xcc -fmodule-map-file=Packages/Kitura-HttpParserHelper-0.3.1/module.modulemap -Xcc -fmodule-map-file=Packages/Kitura-CurlHelpers-0.3.0/module.modulemap -Xcc -fmodule-map-file=Packages/Kitura-Pcre2-0.2.0/module.modulemap
+#swift build -Xcc -fblocks -Xcc -fmodule-map-file=Packages/Kitura-HttpParserHelper-0.3.1/module.modulemap -Xcc -fmodule-map-file=Packages/Kitura-CurlHelpers-0.3.0/module.modulemap -Xcc -fmodule-map-file=Packages/Kitura-Pcre2-0.2.0/module.modulemap
+
+# Copy test credentials for project if available
+if [ -e "Kitura-TestingCredentials/${projectName}" ]; then
+	echo "Found folder with test credentials for ${projectName}."
+  # Copy tests using gradle script
+  ./DevOps/gradle_wrapper/gradlew copyProperties -b copy-project-properties.gradle -PappOpenSourceFolder=${currentDir}/Kitura-TestingCredentials/${projectName} -PappRootFolder=$currentDir
+else
+  echo "No folder found with test credentials for ${projectName}."
+fi
 
 # Execute test cases
-swift test
+# swift test
