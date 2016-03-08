5350290daba7965623401b71f2a3acacd0996445
rolivieri <roliv@us.ibm.com>
2016-03-07 18:53:47 -0600

    IBM-Swift/Kitura#165 New files to support CI builds.

--- Kitura-redis/common/before_tests.sh
+++ Kitura-redis/common/before_tests.sh
@@ -24,15 +24,15 @@ password=$(head -n 1 "${projectFolder}/Tests/SwiftRedisAuth/password.txt")
 echo ">> redis password: $password"
 
 # Update redis password
-#perl -pi -e "s/# requirepass foobared/requirepass ${password}/g" $REDIS_CONF_FILE
+perl -pi -e "s/# requirepass foobared/requirepass ${password}/g" $REDIS_CONF_FILE
 
-#echo ">> Contents of ${REDIS_CONF_FILE} next:"
-#cat $REDIS_CONF_FILE
+echo ">> Contents of ${REDIS_CONF_FILE} next:"
+cat $REDIS_CONF_FILE
 
 # Start redis server
 redis-server &
+sleep 10
 
-sleep 25
-
+#sleep 25
 # Set redis password
-redis-cli CONFIG SET requirepass "$password"
+#redis-cli CONFIG SET requirepass "$password"
