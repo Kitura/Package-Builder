cf4310d27cbf89ea1480dd6353511ae8827a0021
rolivieri <roliv@us.ibm.com>
2016-04-04 16:33:44 -0500

    Added support for DEVELOPMENT-SNAPSHOT-2016-03-24-a.

--- build_kitura_package.sh
+++ build_kitura_package.sh
@@ -24,7 +24,7 @@
 set -e
 
 # Swift version for build
-export SWIFT_SNAPSHOT=swift-DEVELOPMENT-SNAPSHOT-2016-03-01-a
+export SWIFT_SNAPSHOT=swift-DEVELOPMENT-SNAPSHOT-2016-03-24-a
 echo ">> SWIFT_SNAPSHOT: $SWIFT_SNAPSHOT"
 export WORK_DIR=/root
 
