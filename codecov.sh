fa26c92eb54ad44d8876d629dff0a15bfba21e7b
Navneet Gupta <navneet@us.ibm.com>
2016-12-15 11:13:23 -0600

    Generate code coverage for builds targeting master branch

--- codecov.sh
+++ codecov.sh
@@ -1,7 +1,7 @@
 #! /bin/bash
 
-if [[ $TRAVIS && $TRAVIS_EVENT_TYPE != "cron" ]]; then
-    echo "Not cron build. Skipping code coverage generation"
+if [[ $TRAVIS && $TRAVIS_BRANCH != "master" && $TRAVIS_EVENT_TYPE != "cron" ]]; then
+    echo "Not master or cron build. Skipping code coverage generation"
     exit 0
 fi
 
