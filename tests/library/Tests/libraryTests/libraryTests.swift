d1d3ca9bb323574e8c75754af78038250dd67cce
Christian Compton <cbcompto@us.ibm.com>
2017-05-08 14:17:31 -0500

    swiftlint update

--- tests/library/Tests/libraryTests/libraryTests.swift
+++ tests/library/Tests/libraryTests/libraryTests.swift
@@ -5,10 +5,10 @@ class LibraryTests: XCTestCase {
     func testExample() {
         // This is an example of a functional test case.
         // Use XCTAssert and related functions to verify your tests produce the correct results.
-        XCTAssertEqual(library().text, "Hello, World!")
+        XCTAssertEqual(Library().text, "Hello, World!")
     }
 
-    static var allTests: [(String, (libraryTests) -> () throws -> Void)] {
+    static var allTests: [(String, (LibraryTests) -> () throws -> Void)] {
         return [
             ("testExample", testExample)
         ]
