244c7d9e808a65bfc36c352eafd0f2a861167763
Christian Compton <cbcompto@us.ibm.com>
2017-05-11 10:12:09 -0500

    Develop (#109)
    * Swiftlint formatting
    
    * swiftlint update
    
    * swiftlint update
    
    * control for linting
    
    * control test
    
    * control test
    
    * control testing
    
    * swiftlint modifications
    
    * swiftlint modification

--- tests/library/Tests/libraryTests/libraryTests.swift
+++ tests/library/Tests/libraryTests/libraryTests.swift
@@ -1,17 +1,16 @@
 import XCTest
 @testable import library
 
-class libraryTests: XCTestCase {
+class LibraryTests: XCTestCase {
     func testExample() {
         // This is an example of a functional test case.
         // Use XCTAssert and related functions to verify your tests produce the correct results.
-        XCTAssertEqual(library().text, "Hello, World!")
+        XCTAssertEqual(Library().text, "Hello, World!")
     }
 
-
-    static var allTests : [(String, (libraryTests) -> () throws -> Void)] {
+    static var allTests: [(String, (LibraryTests) -> () throws -> Void)] {
         return [
-            ("testExample", testExample),
+            ("testExample", testExample)
         ]
     }
 }
