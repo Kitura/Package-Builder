7dfcf2cf6d88da203e78b2e37c2684a15aa08a06
rolivieri <roliv@us.ibm.com>
2017-07-18 00:26:06 -0500

    Updates to make build process less verbose.

--- codecov.sh
+++ codecov.sh
@@ -1,12 +1,28 @@
 #! /bin/bash
 
+##
+# Copyright IBM Corporation 2016
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+##
+
 if [[ $TRAVIS_BRANCH != "master" && $TRAVIS_BRANCH != "develop" && $TRAVIS_EVENT_TYPE != "cron" ]]; then
     echo "Not master, develop or cron build. Skipping code coverage generation"
     exit 0
 fi
 
 if [[ ${osName} != "osx" ]]; then
-    echo "Not osx build. Skipping code coverage generation"
+    echo "Not a macOS build. Hence, skipping code coverage generation."
     exit 0
 fi
 
@@ -38,8 +54,8 @@ fi
 
 PROJECT="${PROJ_OUTPUT##*/}"
 SCHEME=$(xcodebuild -list -project $PROJECT | grep --after-context=1 '^\s*Schemes:' | tail -n 1 | xargs)
+TEST_CMD="xcodebuild -quiet -project $PROJECT -scheme $SCHEME -sdk $SDK -enableCodeCoverage YES -skipUnavailableActions test"
 
-TEST_CMD="xcodebuild -project $PROJECT -scheme $SCHEME -sdk $SDK -enableCodeCoverage YES -skipUnavailableActions test"
 echo "Running $TEST_CMD"
 eval "$TEST_CMD"
 if [[ $? != 0 ]]; then
@@ -49,14 +65,14 @@ fi
 (( MODULE_COUNT = 0 ))
 BASH_BASE="bash <(curl -s https://codecov.io/bash)"
 for module in $(ls -F Sources/ 2>/dev/null | grep '/$'); do   # get only directories in "Sources/"
-    module=${module%/}                                           # remove trailing slash
+    module=${module%/}                                        # remove trailing slash
     BASH_CMD="$BASH_BASE -J '^${module}\$' -F '${module}'"
     (( MODULE_COUNT++ ))
 
     echo "Running $BASH_CMD"
     eval "$BASH_CMD"
     if [[ $? != 0 ]]; then
-        echo "Error running $BASH_CMD"
+        echo "Error running: $BASH_CMD"
         exit 1
     fi
 done
