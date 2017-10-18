9c1130dacc7b794016c10799a047a41431a11147
Ricardo Olivieri <roliv@us.ibm.com>
2017-10-18 10:58:53 -0500

    Added interim workaround for build issues - ubuntu packages

--- linux/install_swift_binaries.sh
+++ linux/install_swift_binaries.sh
@@ -26,9 +26,10 @@ set -e
 # Echo commands before executing them.
 #set -o verbose
 
+# Interim workaround - https://github.com/travis-ci/travis-ci/issues/8607
+sudo rm -vf /etc/apt/sources.list.d/*riak*
+# Interim workaround
 sudo apt-get -qq update > /dev/null
-# Following line does not work on Bluemix DevOps Pipeline; hence using regular clang install instead.
-#sudo apt-get -y install clang-3.8 lldb-3.8 libicu-dev libtool libcurl4-openssl-dev libbsd-dev build-essential libssl-dev uuid-dev
 sudo apt-get -y -qq install clang lldb-3.8 libicu-dev libtool libcurl4-openssl-dev libbsd-dev build-essential libssl-dev uuid-dev tzdata libz-dev > /dev/null
 
 # Environment vars
