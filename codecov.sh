e876c6e348c6384f6dd8fab850f594bb2f18549a
ShihabMehboob <shihab.mehboob1@ibm.com>
2018-01-29 14:17:11 +0000

    Script updates

--- codecov.sh
+++ codecov.sh
@@ -32,7 +32,7 @@ if [[ $? != 0 ]]; then
 fi
 
 # Determine if there is a custom command for generating xcode project
-cat $(./Package-Builder/generate-xcodeproj.sh)
+source ./Package-Builder/generate-xcodeproj.sh
 
 # Determine if there is a custom command for xcode build (code coverage tests)
 if [ -e ${projectFolder}/.swift-codecov ]; then
