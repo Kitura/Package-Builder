42bb330d74e4b74d9acd88eed845ea0d35d4af11
David Jones <djones6dev@gmail.com>
2018-03-16 11:48:56 +0000

    Complete jazzy-doc auto documentation feature (#133)

--- jazzy.sh
+++ jazzy.sh
@@ -14,31 +14,55 @@
 # limitations under the License.
 ##
 
-# Check that we have credentials
-if [ -z "${GITHUB_USERNAME}" ] && [ -z "${GITHUB_PASSWORD}" ]; then
+# Check that we have credentials. In addition to the username and password,
+# we require an e-mail address for configuring our user when pushing docs
+# updates back to Github.
+if [ -z "${GITHUB_USERNAME}" -o -z "${GITHUB_PASSWORD}" -o -z "${GITHUB_EMAIL}" ]; then
     echo "Supplied jazzy docs flag, but credentials were not provided."
-    echo "Expected: GITHUB_USER && GITHUB_PASSWORD Env variables."
+    echo "Expected: GITHUB_USER, GITHUB_PASSWORD and GITHUB_EMAIL Env variables."
     exit 1
 fi
 
-# Check if .jazzy.yaml exists in the root folder of the repo
-#cd ..
-export projectFolder=`pwd`
-
-if [ -e ./$(projectFolder) ]; then
-
-    # Install jazzy
-    sudo gem install jazzy
-    # Generate xcode project
-    sourceScript "${projectFolder}/generate-xcodeproj.sh"
-    # Run jazzy
-    jazzy
-
-    # Configure endpoint
-    REPO=`git config remote.origin.url`
-    AUTH_REPO=${REPO/https:\/\/github.com\//https://${GITHUB_USERNAME}:${GITHUB_PASSWORD}@github.com/}
-  
-    git add docs/.
-    git commit -m 'Documentation update [ci skip]'
-    git push
+# Check that projectFolder and SCRIPT_DIR exist. These should have been set
+# by the calling script (build-package.sh)
+if [ -z "${projectFolder}" ]; then
+    projectFolder="$pwd"
+    echo "Warning: projectFolder not set. Defaulting to pwd ($projectFolder)"
 fi
+
+if [ -z "${SCRIPT_DIR}" ]; then
+    # Determine location of this script
+    SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+    echo "Warning: SCRIPT_DIR not set. Defaulting to current script location ($SCRIPT_DIR}"
+fi
+
+# Check if .jazzy.yaml exists in the root folder of the repo. If we do not find
+# it, fail the build because the build explicitly requested docs generation.
+if [ ! -f "${projectFolder}/.jazzy.yaml" ]; then
+    echo ".jazzy.yaml file does not exist"
+    exit 1
+fi
+
+# Checkout to the current branch. The repo cloned by Travis is a shallow clone,
+# so we cannot check out the branch directly. Instead, create a new remote for
+# this purpose and checkout the branch from there.
+git remote add jazzy https://${GITHUB_USERNAME}:${GITHUB_PASSWORD}@github.com/${TRAVIS_REPO_SLUG}.git
+git fetch jazzy
+git checkout jazzy/${TRAVIS_PULL_REQUEST_BRANCH} -b ${TRAVIS_PULL_REQUEST_BRANCH}
+
+# Install jazzy
+sudo gem install jazzy
+# Generate xcode project
+sourceScript "${SCRIPT_DIR}/generate-xcodeproj.sh" "xcodeproj generation"
+# Run jazzy
+jazzy
+
+# Configure user
+git config --global --add user.name Auto-Jazzy
+git config --global --add user.email ${GITHUB_EMAIL}
+git config --global --add push.default simple
+
+# Push the updated docs as a new commit to the PR branch
+git add docs/.
+git commit -m 'Documentation update [ci skip]'
+git push
