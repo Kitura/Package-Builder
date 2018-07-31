1ee60552a5f55f742275afc344b65b497f7913b6
Shihab-Mehboob1 <shihab.mehboob1@ibm.com>
2018-07-31 11:12:54 +0100

    Logic and syntax adjustments

--- generate-podspec.sh
+++ generate-podspec.sh
@@ -24,7 +24,7 @@ if [ "$(uname)" == "Darwin" ] && [ $POD_ELIGIBLE ]; then
 
         # Create the podspec file
         podFile="$projectName.podspec"
-        touch "$podFile}
+        touch "$podFile"
         podDirectory=${projectFolder}/${podFile}
 
         # Create and populate the contents of the podspec file
@@ -32,14 +32,16 @@ if [ "$(uname)" == "Darwin" ] && [ $POD_ELIGIBLE ]; then
         podspec="Pod::Spec.new do |s|\ns.name        = \"$projectName\"\ns.version     = \"$TRAVIS_TAG\"\ns.summary     = \"$TRAVIS_DESCRIPTION\"\ns.homepage    = \"https://github.com/IBM-Swift/$projectName\"\ns.license     = { :type => \"Apache License, Version 2.0\" }\ns.author     = \"IBM\"\ns.module_name  = '$projectName'\ns.requires_arc = true\ns.ios.deployment_target = \"10.0\"\ns.source   = { :git => \"https://github.com/IBM-Swift/$projectName.git\", :tag => s.version }\ns.source_files = \"Sources/$projectName/*.swift\"\ns.pod_target_xcconfig =  {\n'SWIFT_VERSION' => '4.0.3',\n}"
 
         # Check that a Package.swift file exists, extract dependencies, and use within the podspec file
-        if [ -e "$Package.swift" ]; then
+        if [ -e "Package.swift" ]; then
             echo "Package.swift file found, may contain dependencies."
             # Get and append name of dependencies from the Package.swift file
-            # Need a loop to iterate over all dependencies found - TO DO
-            dependency = sed 's/.*IBM-Swift\/\(.*\).git.*/\1/' <<< $(cat "Package.swift")
-            podspec = podspec + "\ns.dependency '$dependency'\nend"
+            dependencies=$(grep -Eo "IBM-Swift/(.+?).git" Package.swift | sed -E "s|IBM-Swift/||g" | sed -E "s|.git||g")
+            while read -r line; do
+                podspec="$podspec\ns.dependency '$line'"
+            done <<< "$dependencies"
+            podspec="$podspec\nend"
         else
-            podspec = podspec + "\nend"
+            podspec="$podspec\nend"
         fi
 
         # Copy the variable contents to the podspec file
@@ -60,7 +62,6 @@ if [ "$(uname)" == "Darwin" ] && [ $POD_ELIGIBLE ]; then
         git push
 
         # Register a session
-        # Needs replacing with default IBM credentials - TO DO
         pod trunk register swift@ibm.com 'IBM' --description='IBM-Dev'
 
         # Upload the podspec to the Cocoapods Spec
