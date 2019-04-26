4b16e32ceb7e0f05bd9be84ddfa1c822720b5274
David Jones <djones6dev@gmail.com>
2019-04-26 10:51:12 +0100

    Install specific dependencies for Swift based on version (#165)
    * Install specific dependencies for Swift based on version
    * Customize libcurl package name based on OS release

--- linux/install_swift_binaries.sh
+++ linux/install_swift_binaries.sh
@@ -31,15 +31,66 @@ echo ">> Running ${BASH_SOURCE[0]}"
 # Suppress prompts of any kind while executing apt-get
 export DEBIAN_FRONTEND="noninteractive"
 
-sudo -E apt-get -q update
-sudo -E apt-get -y -q install clang lldb-3.8 libicu-dev libtool libcurl4-openssl-dev libbsd-dev build-essential libssl-dev uuid-dev tzdata libz-dev libblocksruntime-dev
+# Find the first numeric value in the swift version string. For example:
+# swift-5.0-DEVELOPMENT-SNAPSHOT-1234-56-78-a  -> 5
+# 4.0.3 -> 4
+swiftMajor=`echo $SWIFT_SNAPSHOT | sed -e's#[^0-9]*\([0-9]\).*#\1#'`
 
-# Get the ID and VERSION_ID from /etc/os-release, stripping quotes
+# Get the Ubuntu ID and VERSION_ID from /etc/os-release, stripping quotes
 distribution=`grep '^ID=' /etc/os-release | sed -e's#.*="\?\([^"]*\)"\?#\1#'`
 version=`grep '^VERSION_ID=' /etc/os-release | sed -e's#.*="\?\([^"]*\)"\?#\1#'`
 version_no_dots=`echo $version | awk -F. '{print $1$2}'`
+versionMajor=`echo $version | awk -F. '{print $1}'`
 export UBUNTU_VERSION="${distribution}${version}"
 export UBUNTU_VERSION_NO_DOTS="${distribution}${version_no_dots}"
+
+# Customize package dependencies based on Ubuntu version
+case $versionMajor in
+14|15|16|17)
+  libCurlPackage="libcurl3"
+  ;;
+*)
+  libCurlPackage="libcurl4"
+  ;;
+esac
+
+# Install prerequisites for this version of Swift
+sudo -E apt-get -q update
+case $swiftMajor in
+3|4)
+  sudo -E apt-get -y -q install \
+    libcurl4-openssl-dev \
+    libssl-dev \
+    clang \
+    lldb-3.8 \
+    libicu-dev \
+    libtool \
+    libbsd-dev \
+    build-essential \
+    uuid-dev \
+    tzdata \
+    libz-dev \
+    libblocksruntime-dev
+  ;;
+*)
+  sudo -E apt-get -y -q install \
+    libcurl4-openssl-dev \
+    libssl-dev \
+    libatomic1 \
+    libbsd0 \
+    ${libCurlPackage} \
+    libxml2 \
+    libedit2 \
+    libsqlite3-0 \
+    libc6-dev \
+    binutils \
+    libgcc-5-dev \
+    libstdc++-5-dev \
+    libpython2.7 \
+    tzdata \
+    pkg-config
+  ;;
+esac
 
 echo ">> Installing '${SWIFT_SNAPSHOT}'..."
 # Install Swift compiler
