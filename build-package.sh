0e24fadac9172ee7bb46297820a2c40c1f3135df
David Jones <djones6@uk.ibm.com>
2019-06-25 13:52:15 +0100

    Clean up containers so that subsequent builds do not conflict

--- build-package.sh
+++ build-package.sh
@@ -137,11 +137,14 @@ if [ -n "${DOCKER_IMAGE}" ]; then
   set -x
   docker run ${docker_run_privileged} ${docker_env_vars} -v ${projectBuildDir}:${projectBuildDir} --name packagebuildercontainer ${DOCKER_IMAGE} /bin/bash -c "$docker_python_fix && apt-get update && apt-get install -y ${docker_pkg_list}"
   docker commit packagebuildercontainer packagebuilderimage
+  docker container rm packagebuildercontainer
   set +x
   travis_end
   # Run Package-Builder within the new image.
   set -x
-  docker run -v ${projectBuildDir}:${projectBuildDir} packagebuilderimage /bin/bash -c "cd $projectBuildDir && ./Package-Builder/build-package.sh ${PACKAGE_BUILDER_ARGS}"
+  docker run -v ${projectBuildDir}:${projectBuildDir} --name packagebuilderrun packagebuilderimage /bin/bash -c "cd $projectBuildDir && ./Package-Builder/build-package.sh ${PACKAGE_BUILDER_ARGS}"
+  docker container rm packagebuilderrun
+  docker image rm packagebuilderimage
   set +x
   DOCKER_RC=$?
   echo ">> Docker execution complete, RC=${DOCKER_RC}"
